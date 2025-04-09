51e5b597f9f0552f569adbf1b9245a4e0f393b2a
rfrankeltcsbell <bellmore+rfrankel@thecoderschool.com>
2025-04-09 23:31:41 +0000

    added moods

--- mood.py
+++ mood.py
@@ -24,11 +24,15 @@ def show_mood_chart():
     dates= list(data.keys())
     moods = list(data.values())
     mood_levels= {
+        "surprised":8,
+        "bored":7,
+        "excited":6,
         "happy": 5,
         "okay": 4,
         "tired": 3,
         "angry": 2,
         "sad": 1,
+        
     }
     yvalues= [mood_levels.get(mood,0)for mood in moods]
 
